6ddc038080ea76d4e51e339a14e6e5d2d55045c7
Allan Oliveira <r9oliveira@gmail.com>
2013-12-01 23:01:19 +0800

    Added Todo Model.

---
+++ asp-example/Models/Todo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace asp_example.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public bool Archived { get; set; }
+        public DateTime Created { get; set; }
+
+        public Todo()
+        {
+            Created = DateTime.Now;
+        }
+    }
+}
