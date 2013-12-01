0f4fc7dde9b54452edc27cec64751a0c215f6db2
Allan Oliveira <r9oliveira@gmail.com>
2013-12-01 23:12:33 +0800

    Added basic addition methods to HomeController plus UI and style changes.

--- asp-example/Controllers/HomeController.cs
+++ asp-example/Controllers/HomeController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using asp_example.Controllers.ViewModels;
+using asp_example.Models;
+using asp_example.Models.Context;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,9 +13,36 @@ namespace asp_example.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            var vm = new HomeViewModel();
 
-            return View();
+            // TODO: Move to repo and use autofac instead
+            using (var db = new TodoContext())
+            {
+                var items = db.Todos;
+
+                vm.AddItems(items.ToList());
+            }
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HomeViewModel vm)
+        {
+            if (!ModelState.IsValid)
+                return View(vm);
+
+            var newVm = new HomeViewModel();
+            // Move to repo and use injector to map from vm to model
+            using (var db = new TodoContext())
+            {
+                db.Todos.Add(new Todo { Description = vm.Description });
+                db.SaveChanges();
+
+                newVm.AddItems(db.Todos.ToList());
+            }
+
+            return View(newVm);
         }
 
         public ActionResult About()
