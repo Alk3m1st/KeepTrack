efc9c7b8b2960756f5d88c5cb7727a909b6b0a0e
Allan Oliveira <r9oliveira@gmail.com>
2013-12-02 23:49:49 +0800

    Added DateDisplayHelpers.

---
+++ asp-example.tests/Utils/DateDisplayHelpersTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using asp_example.Utils;
+
+namespace asp_example.tests
+{
+    [TestFixture]
+    public class DateDisplayHelpersShould
+    {
+        [TestCase(0)]
+        [TestCase(-1)]
+        [TestCase(-2)]
+        [TestCase(-3)]
+        [TestCase(-4)]
+        public void Display_the_correct_elapsed_class(int daysPassed)
+        {
+            var createdDate = DateTime.Now.AddDays(daysPassed);
+
+            Assert.That(createdDate.GetElapsedDaysClass(), Is.EqualTo("dfs"));
+        }
+    }
+}
---
+++ asp-example/Utils/DateDisplayHelpers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asp_example.Utils
+{
+    public static class DateDisplayHelpers
+    {
+        public static string GetNiceDateFormat(this DateTime date)
+        {
+            if (date.Date.Equals(DateTime.Now.Date))
+                return "Today";
+
+            if (date.Date.AddDays(7) > DateTime.Now.Date)
+                return date.DayOfWeek.ToString();
+
+            return date.Date.ToShortDateString();
+        }
+
+        public static string GetElapsedDaysClass(this DateTime date)
+        {
+            return date.Date.AddDays(4) > DateTime.Now.Date ? "three-days-elapsed" :
+                date.Date.AddDays(3) > DateTime.Now.Date ? "two-days-elapsed" :
+                date.Date.AddDays(2) > DateTime.Now.Date ? "one-day-elapsed" :
+                string.Empty;
+        }
+    }
+}
