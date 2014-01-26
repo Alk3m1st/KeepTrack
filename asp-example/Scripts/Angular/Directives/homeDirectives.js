62db0fa4b57d43418de7e2c3eabcc44eb9d5bfaa
Allan Oliveira <r9oliveira@gmail.com>
2014-01-26 22:26:57 +0800

    Added directive to item actions.

---
+++ asp-example/Scripts/Angular/Directives/homeDirectives.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+window.app.directive('itemActions', function () {
+    return {
+        restrict: 'A',
+        replace: true,
+        template: '<form action="#" method="post">' +
+            '<input type="hidden" value="{{item.Id}}" name="id">' +
+            '<input type="submit" value="Done" name="Archive" data-ng-click="">' +
+            '</form>',
+        controller: 'homeController'
+    }
+});
