bce81ace0bc184275ec84987bc4a662029260823
Allan Oliveira <r9oliveira@gmail.com>
2014-07-09 20:48:59 +0800

    Added TableStorageHelpers.

---
+++ asp-example.tests/Utils/TableStorageHelpersTests.cs
@@ -0,0 +1,26 @@
+﻿using asp_example.Utils;
+using Microsoft.WindowsAzure.Storage;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asp_example.tests.Utils
+{
+    [TestFixture]
+    public class TableStorageHelpersShould
+    {
+        [Test]
+        public void Get_table()
+        {
+            var storageConnectionString = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+
+            var table = TableStorageHelpers.GetTable(storageConnectionString, "Todos");
+
+            Assert.That(table, Is.Not.Null);
+        }
+    }
+}
---
+++ asp-example/Utils/TableStorageHelpers.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asp_example.Utils
+{
+    public static class TableStorageHelpers
+    {
+        public static CloudTable GetTable(this CloudStorageAccount storageAccount, string tableName)
+        {
+            // Create the table client
+            var cloudTableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the table if it doesn't exist
+            var table = cloudTableClient.GetTableReference(tableName.ToLowerInvariant());
+            table.CreateIfNotExists();
+
+            return table;
+        }
+        
+        public static CloudTable GetTable<T>(this CloudStorageAccount storageAccount) where T : TableEntity
+        {
+            // Create the table client
+            var cloudTableClient = storageAccount.CreateCloudTableClient();
+
+            // Create the table if it doesn't exist
+            var table = cloudTableClient.GetTableReference(typeof(T).Name.ToLowerInvariant());
+            table.CreateIfNotExists();
+
+            return table;
+        }
+    }
+}
