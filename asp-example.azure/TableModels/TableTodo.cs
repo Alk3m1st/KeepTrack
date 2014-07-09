e534dbaaa28591ea4a68be7963a7de0ca2a49d6b
Allan Oliveira <r9oliveira@gmail.com>
2014-07-09 20:14:38 +0800

    Added asp-example.azure project.

---
+++ asp-example.azure/TableModels/TableTodo.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asp_example.models.TableModels
+{
+    public class TableTodo : TableEntity
+    {
+        public Guid Id { get; set; }
+        public string Description { get; set; }
+        public bool Archived { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Completed { get; set; }
+        public int DisplayOrder { get; set; }
+        public int UserId { get; set; }
+        public bool Deleted { get; set; }
+
+        public TableTodo()
+        {
+            Created = DateTime.Now;
+        }
+
+        public TableTodo(int userId, string description) : this()
+        {
+            this.Id = new Guid();
+            this.RowKey = this.Id.ToString();
+
+            this.UserId = userId;
+            this.PartitionKey = this.UserId.ToString();
+
+            this.Description = description;
+        }
+    }
+}
