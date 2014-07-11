e7de9ce3fd832397d8368948a40a5a26f5125396
Allan Oliveira <r9oliveira@gmail.com>
2014-07-11 20:42:09 +0800

    Add TableTodoRepository.

---
+++ asp-example/Repositories/TableTodoRepository.cs
@@ -0,0 +1,33 @@
+﻿using asp_example.Utils;
+using asp_example.models.TableModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace asp_example.Repositories
+{
+    public interface ITableTodoRepository : ITableStorageRepository//<TableTodo>
+    {
+        void Archive(Guid id, string userId);
+    }
+
+    public class TableTodoRepository : TableStorageRepository<TableTodo>, ITableTodoRepository
+    {
+        public void Archive(Guid id, string userId)
+        {
+            var tableClient = StorageAccount.GetTable<TableTodo>();
+
+            var retrieveOperation = TableOperation.Retrieve<TableTodo>(userId.ToString(), id.ToString());
+            var entity = tableClient.Execute(retrieveOperation).Result as TableTodo;
+
+            if(entity == null)
+                return;
+
+            entity.Archived = true;
+
+            base.InsertOrReplace(entity);
+        }
+    }
+}
