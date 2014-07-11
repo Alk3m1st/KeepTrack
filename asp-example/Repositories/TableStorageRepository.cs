15c48343a5a7fce61882fb15409e64b33a9630c5
Allan Oliveira <r9oliveira@gmail.com>
2014-07-11 20:39:56 +0800

    Make storage account protected and add delete method to TableStorageRepository.

--- asp-example/Repositories/TableStorageRepository.cs
+++ asp-example/Repositories/TableStorageRepository.cs
@@ -13,41 +13,41 @@ namespace asp_example.Repositories
     public interface ITableStorageRepository
     {
         void Insert<T>(T entity) where T : TableEntity;
-        T Get<T>(Guid id, int userId) where T : TableEntity;
+        T Get<T>(Guid id, string userId) where T : TableEntity;
         IList<T> Get<T>(string userId) where T : TableEntity, new();
         void InsertOrReplace<T>(T entity) where T : TableEntity;
+        void Delete<T>(Guid id, string userId) where T : TableEntity;
     }
 
-    public class TableStorageRepository : ITableStorageRepository
+    public class TableStorageRepository<T> : ITableStorageRepository//<T>
     {
-        private CloudStorageAccount _storageAccount;
+        protected CloudStorageAccount StorageAccount;
 
         public TableStorageRepository()
         {
-            _storageAccount = CloudStorageAccount.Parse(
+            StorageAccount = CloudStorageAccount.Parse(
                 ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
         }
 
         public void Insert<T>(T entity) where T : TableEntity
         {
-            var tableClient = _storageAccount.GetTable<T>();
+            var tableClient = StorageAccount.GetTable<T>();
 
             var insertOperation = TableOperation.Insert(entity);
             tableClient.Execute(insertOperation);
         }
 
-        public T Get<T>(Guid id, int userId) where T : TableEntity
+        public T Get<T>(Guid id, string userId) where T : TableEntity
         {
-            var tableClient = _storageAccount.GetTable<T>();
+            var tableClient = StorageAccount.GetTable<T>();
 
             var retrieveOperation = TableOperation.Retrieve<T>(userId.ToString(), id.ToString());
-
             return tableClient.Execute(retrieveOperation).Result as T;
         }
 
         public IList<T> Get<T>(string userId) where T : TableEntity, new()
         {
-            var tableClient = _storageAccount.GetTable<T>();
+            var tableClient = StorageAccount.GetTable<T>();
 
             var query = new TableQuery<T>()
                 .Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, userId.ToString()));
@@ -58,10 +58,25 @@ namespace asp_example.Repositories
         // TODO: Remove this and use event sourcing
         public void InsertOrReplace<T>(T entity) where T : TableEntity
         {
-            var tableClient = _storageAccount.GetTable<T>();
+            var tableClient = StorageAccount.GetTable<T>();
 
             var insertOperation = TableOperation.InsertOrReplace(entity);
             tableClient.Execute(insertOperation);
+        }
+
+        public void Delete<T>(Guid id, string userId) where T : TableEntity
+        {
+            // TODO: Don't actually delete!
+            var tableClient = StorageAccount.GetTable<T>();
+
+            var retrieveOperation = TableOperation.Retrieve<T>(userId.ToString(), id.ToString());
+            var entity = tableClient.Execute(retrieveOperation).Result as T;
+
+            if (entity == null)
+                return;
+
+            var deleteOperation = TableOperation.Delete(entity);
+            tableClient.Execute(deleteOperation);
         }
     }
 }
