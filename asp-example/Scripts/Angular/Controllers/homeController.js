30d26aaa3059e5aec4c9ca05d9312a66d958be0e
Allan Oliveira <r9oliveira@gmail.com>
2014-12-24 16:48:46 +0800

    Refactored homeController.js to use 'Controller As' functionality.

--- asp-example/Scripts/Angular/Controllers/homeController.js
+++ asp-example/Scripts/Angular/Controllers/homeController.js
@@ -3,18 +3,21 @@
 (function () {
     function HomeController($scope, $http) {
         // Init
-        $scope.todos = [];
-        $scope.description = '';
+        var self = this;
+        this.todos = [];
+        this.description = '';
 
         // TODO: Add caching with cacheFactory
         $http.get('/HomeJson')
             .success(function (data) {
-                $scope.todos = data.TodoItems;
+                self.todos = data.TodoItems;
             });
 
-        $scope.itemsClass = "showItems";
+        this.itemsClass = "showItems";  // needed?
 
-        $scope.addTodo = function () {
+        // Functions
+        this.addTodo = function () {
+
             // Put in service
             if (this.description) {
                 var item = { Description: this.description };
@@ -22,20 +25,18 @@
                 $http.post('/HomeJson/AddTodo', item)
                     .success(function (newItem) {
                         // TODO: Add display order and filter by it?
-                        $scope.todos.splice(0, 0, newItem); // Put it at the top
-
-                        //$scope.description = '';
+                        self.todos.splice(0, 0, newItem); // Put it at the top
                     })
                     .error(function (x) {
                         console.log('fail');
                         console.log(x);
                     });
 
-                this.description = '';
+                self.description = '';
             }
         };
 
-        $scope.archive = function (item) {
+        this.archive = function (item) {
             item.hide = true;
 
             // TODO: Move to service
@@ -45,19 +46,19 @@
                     console.log("returned");
 
                     // Reorder the list
-                    for (var i = 0; i < $scope.todos.length; i++) {
-                        if ($scope.todos[i].Id == item.Id) {
+                    for (var i = 0; i < self.todos.length; i++) {
+                        if (self.todos[i].Id == item.Id) {
                             indexToRemove = i;
                         }
 
-                        if ($scope.todos[i].Archived === true) {
+                        if (self.todos[i].Archived === true) {
                             indexToInsert = i;
                         }
 
                         if (indexToRemove >= 0 && indexToInsert > 0) {
-                            $scope.todos.splice(indexToRemove, 1);
+                            self.todos.splice(indexToRemove, 1);
 
-                            $scope.todos.splice(indexToInsert - 1, 0, updatedItem);
+                            self.todos.splice(indexToInsert - 1, 0, updatedItem);
 
                             return;
                         }
@@ -70,23 +71,23 @@
                 });
         };
 
-        $scope.delete = function (item) {
+        this.delete = function (item) {
             item.hide = true;
 
-            // pop-up to verify delete somehow...
+            // pop-up to verify delete ?...
 
             // TODO: Move to service
             $http.post('/HomeJson/Delete', item)
                 .success(function () {
                     var indexToRemove = 0;
 
-                    for (var i = 0; i < $scope.todos.length; i++) {
-                        if ($scope.todos[i].Id == item.Id) {
+                    for (var i = 0; i < self.todos.length; i++) {
+                        if (self.todos[i].Id === item.Id) {
                             indexToRemove = i;
                         }
 
-                        if (indexToRemove > 0) {
-                            $scope.todos.splice(indexToRemove, 1);
+                        if (indexToRemove >= 0) {
+                            self.todos.splice(indexToRemove, 1);
 
                             return;
                         }
