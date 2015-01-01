c01df3fbfd56af7e9c0bf1b060810d1eaa17c191
Allan Oliveira <r9oliveira@gmail.com>
2015-01-01 11:19:34 +0800

    Added Query interfaces

---
+++ asp-example.interfaces/Queries/IQueryHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asp_example.interfaces.Queries
+{
+    public interface IQueryHandler<TQuery, TResult> where TQuery : IQuery<TResult>
+    {
+        TResult Handle(TQuery query);
+    }
+}
