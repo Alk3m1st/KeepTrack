3ffe7e05a975dca2d4e54feda7060215977137b2
Allan Oliveira <r9oliveira@gmail.com>
2015-01-03 22:43:36 +0800

    Added AddTodoCommand.cs

---
+++ asp-example/Commands/AddTodoCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asp_example.Commands
+{
+    public class AddTodoCommand
+    {
+        public string UserName { get; private set; }
+        public string Description { get; private set; }
+        public Guid Id { get; private set; }
+
+        public AddTodoCommand(string userName, string description)
+        {
+            UserName = userName;
+            Description = description;
+            Id = Guid.NewGuid();
+        }
+    }
+}
