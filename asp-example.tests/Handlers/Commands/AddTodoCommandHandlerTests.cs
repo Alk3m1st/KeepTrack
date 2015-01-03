bb3488195f7deeca5f2c9a55c1789e8e1ef922ed
Allan Oliveira <r9oliveira@gmail.com>
2015-01-03 22:47:41 +0800

    Added AddTodoCommandHandler.

---
+++ asp-example.tests/Handlers/Commands/AddTodoCommandHandlerTests.cs
@@ -0,0 +1,46 @@
+﻿using asp_example.Commands;
+using asp_example.Handlers.Commands;
+using asp_example.models.TableModels;
+using asp_example.Utils;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asp_example.tests.Handlers.Commands
+{
+    [TestFixture]
+    public class AddTodoCommandHandlerTests
+    {
+        private CloudStorageAccount StorageAccount;
+        private const string UserName = "bob";
+
+        [SetUp]
+        public void SetUp()
+        {
+            StorageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+        }
+
+        [Test]
+        public void HandleCommand()
+        {
+            var command = new AddTodoCommand(UserName, "Description");
+            var handler = new AddTodoCommandHandler();
+
+            handler.Handle(command);
+
+            var tableClient = StorageAccount.GetTable<TableTodo>();
+            var retrieveOperation = TableOperation.Retrieve<TableTodo>(command.UserName, command.Id.ToString());
+            var entity = tableClient.Execute(retrieveOperation).Result as TableTodo;
+
+            Assert.That(entity, Is.Not.Null);
+            Assert.That(entity.UserId, Is.EqualTo(UserName));
+            Assert.That(entity.Id, Is.EqualTo(command.Id));
+        }
+    }
+}
---
+++ asp-example/Handlers/Commands/AddTodoCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using asp_example.Commands;
+using asp_example.interfaces.Commands;
+using asp_example.models.TableModels;
+using asp_example.Utils;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace asp_example.Handlers.Commands
+{
+    public class AddTodoCommandHandler : ICommandHandler<AddTodoCommand>
+    {
+        protected CloudStorageAccount StorageAccount;
+
+        public AddTodoCommandHandler()
+        {
+            StorageAccount = CloudStorageAccount.Parse(
+                ConfigurationManager.ConnectionStrings["StorageConnectionString"].ConnectionString);
+        }
+
+        public void Handle(AddTodoCommand command)
+        {
+            var todo = new TableTodo(command.UserName, command.Description, command.Id);
+            var tableClient = StorageAccount.GetTable<TableTodo>();
+
+            var insertOperation = TableOperation.Insert(todo);
+            tableClient.Execute(insertOperation);
+        }
+    }
+}
