fbbc6f380da144e99d158016f33e5df97ff2cf73
Allan Oliveira <r9oliveira@gmail.com>
2015-01-03 22:30:14 +0800

    Added ICommandHandler interface.

---
+++ asp-example.interfaces/Commands/ICommandHandler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asp_example.interfaces.Commands
+{
+    public interface ICommandHandler<TCommand>
+    {
+        void Handle(TCommand command);
+    }
+}
