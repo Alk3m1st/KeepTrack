cde0020ae00ff4f227d9a38e78f78e4e9f46e9fe
Allan Oliveira <r9oliveira@gmail.com>
2015-01-03 22:55:17 +0800

    Add CommandHandlersModule.

---
+++ asp-example/Config/AutoFac/CommandHandlersModule.cs
@@ -0,0 +1,20 @@
+﻿using asp_example.Commands;
+using asp_example.Handlers.Commands;
+using asp_example.interfaces.Commands;
+using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace asp_example.Config.AutoFac
+{
+    public class CommandHandlersModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            // TODO: Make more generic
+            builder.RegisterType<AddTodoCommandHandler>().As<ICommandHandler<AddTodoCommand>>().InstancePerLifetimeScope();
+        }
+    }
+}
